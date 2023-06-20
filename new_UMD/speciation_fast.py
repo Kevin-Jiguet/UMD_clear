0dceef80cb6486f6081fa85d886b2a7fc577e99c
Kevin-Jiguet <129768379+Kevin-Jiguet@users.noreply.github.com>
2023-06-20 17:23:12 +0200

    Update speciation_fast.py
    Now uses the centralized umd_processes_fast.py library.

--- new_UMD/speciation_fast.py
+++ new_UMD/speciation_fast.py
@@ -18,6 +18,7 @@ from functools import partial
 import concurrent.futures
 from os.path import join
 import ctypes
+import umd_processes_fast as umdpf
 
 
 current_path=os.path.abspath(__file__)#For all this to work, the files < c_clusters_B.so > and < c_define_bonds.so > must be in the same directory than gofr_umd
@@ -30,10 +31,6 @@ for u in path_red:
 clust_lib = ctypes.cdll.LoadLibrary(join(path_new, 'c_clusters_B.so'))
 clust_lib.fullclustering.argtypes = [ctypes.POINTER(ctypes.c_int),ctypes.POINTER(ctypes.c_int),ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_int]
 clust_lib.fullclustering.restype = ctypes.POINTER(ctypes.c_int)
-
-bond_lib = ctypes.cdll.LoadLibrary(join(path_new, 'c_define_bonds.so'))
-bond_lib.defineBonds.argtypes = [ctypes.POINTER(ctypes.c_char),ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_int]
-bond_lib.defineBonds.restype = ctypes.POINTER(ctypes.c_int)
 
 def analysis_subtab(Clusters,Step):#Creates a dictionnary whose keys are the individual clusters, and the values are a list of list of their *consecutive* steps of existence. 
         population_parallel={}
@@ -76,103 +73,28 @@ def clustering(SnapshotBonds,SnapshotBondIndexes,step,CentMin,CentMax,OutMin,Out
         return []
         
     return Clusters
-    
-def prep_read(BondFile):#Returns the indexes of octets pertaining to each snapshot in the bonding file ; builds the Crystal
-    MyCrystal = cr.Lattice()
-    ff=open(BondFile,"r")
-    for _ in range(20):
-        line = ff.readline()
-        if not line: break
-            #print(line,len(line))
-        if len(line) > 1:
-            line=line.strip()
-            entry=line.split()
-            if entry[0] == 'natom':
-                MyCrystal.natom = int(entry[1])
-                MyCrystal.typat = [0 for _ in range(MyCrystal.natom)]
-            if entry[0] == 'ntypat':
-                MyCrystal.ntypat = int(entry[1])
-                MyCrystal.types = [0 for _ in range(MyCrystal.ntypat)]
-                MyCrystal.elements = ['X' for _ in range(MyCrystal.ntypat)]
-                MyCrystal.masses = [1.0 for _ in range(MyCrystal.ntypat)]
-                MyCrystal.zelec = [1.0 for _ in range(MyCrystal.ntypat)]
-            if entry[0] == 'types':
-                for ii in range(MyCrystal.ntypat):
-                    MyCrystal.types[ii]=int(entry[ii+1])
-            if entry[0] == 'elements':
-                for ii in range(MyCrystal.ntypat):
-                    MyCrystal.elements[ii]=entry[ii+1]
-            if entry[0] == 'masses':
-                for ii in range(MyCrystal.ntypat):
-                    MyCrystal.masses[ii]=float(entry[ii+1])
-            if entry[0] == 'Zelectrons':
-                for ii in range(MyCrystal.ntypat):
-                    MyCrystal.zelec[ii]=float(entry[ii+1])
-            if entry[0] == 'typat':
-                for ii in range(MyCrystal.natom):
-                    MyCrystal.typat[ii] = int(entry[ii+1])
-            if entry[0] == 'timestep':
-                TimeStep = float(entry[1])
-    
-    OctIndexes = []
-    while True :
-        line = ff.readline()
-        if not line :
-            break
-        line=line.strip().split()
-        if line[0]=="step":
-            print("preparing snapshot n. "+line[1]+" to be read")
-            OctIndexes.append(ff.tell())
-            for _ in range(MyCrystal.natom):
-                ff.readline()
-    OctIndexes.append(ff.tell())
-    ff.close()
-    return MyCrystal, OctIndexes, TimeStep
-
-def read_snapshotbonds_C(octettop,octetbot,step,CentMin,CentMax,OutMin,OutMax,File):#Converts the bonding information of a given snapshot into a list of atoms 
-    print("extracting bonds from snapshot n. "+str(step))                           #the data is contained between the octets "octettop" and "octetbot" in the bonding file
-    
-    ff=open(File,"r")
-    ff.seek(octettop,0)
-    snapshot=ff.read(octetbot-octettop)
-    ff.close()
-
-    snapPointer = ctypes.c_char_p(snapshot.encode('utf-8'))
-    
-    BList = bond_lib.defineBonds(snapPointer,len(snapshot),CentMin,CentMax,OutMin,OutMax)
-    
-    SnapshotBondingTable = []
-    SnapshotBondIndexes = []
-   
-    #Converts the C data into two python lists
-    for i in range(2,BList[0]+1):
-        SnapshotBondingTable.append(BList[i])#This one contains the atoms
-    for i in range(BList[0]+2,BList[0]+BList[1]+2):
-        SnapshotBondIndexes.append(BList[i])#This one contains the information about which is bound to which in the other list
-        
-    return SnapshotBondingTable,SnapshotBondIndexes
 
 def main(argv):
     umdp.headerumd()
     BondFile='bonding.umd.dat'
     Nsteps = 1
     ClusterAtoms = []
-    Cations = []
-    Anions = []
     minlife = 5
+    Central=""
+    Adjacent=""
     rings = 1
     header = ''
-    nChunks=8
+    nChunks = 8
     start=time.time()
     try:
-        opts, arg = getopt.getopt(argv,"hf:s:c:a:m:r:n:",["fBondFile","sSampling_Frequency", "cCations","aAnions","mMinlife","rRings","nnChunks"])
+        opts, arg = getopt.getopt(argv,"hf:s:c:a:m:r:n:",["fBondFile","sSampling_Frequency", "cCentral","aAdjacent","mMinlife","rRings","nnChunks"])
     except getopt.GetoptError:
-        print ('speciation.py -f <bond_filename> -s <Sampling_Frequency> -c <Cations> -a <Anions> -m <MinLife> -r <Rings> -n <nChunks>')
+        print ('speciation.py -f <bond_filename> -s <Sampling_Frequency> -c <Central atoms> -a <Adjacent atoms> -m <MinLife> -r <Rings> -n <nChunks>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
             print ('speciation_fast.py program to identify speciation')
-            print ('speciation_fast.py -f <Bonding_filename> -s <Sampling_Frequency> -l <MaxLength> -c <Cations> -a <Anions> -m <MinLife> -r <Rings> -n <nChunks>')
+            print ('speciation_fast.py -f <Bonding_filename> -s <Sampling_Frequency> -l <MaxLength> -c <Central atoms> -a <Adjacent atoms> -m <MinLife> -r <Rings> -n <nChunks>')
             print ('  default values: -f bonding.umd.dat -s 1 -m 0 -r 1 -n 4')
             print (' the bond file contains the bonds relations for each snapshot. Computed with Bond_par_C.py.')
             print (' -r : rings = 1 default, all anions bind to cations ; rings = 0, polymerization, all anions bind to cations AND anions ; rings = x>0, all anions bind to cations then to other anions to form a xth-coordination polyhedra')
@@ -190,14 +112,12 @@ def main(argv):
             minlife = float(arg)
         elif opt in ("-n","--nnChunks"):
             nChunks = int(arg)
-        elif opt in ("-c","--Cations"):
+        elif opt in ("-c","--Central"):
             header = header + ' -c=' + arg
-            Cations = arg.split(",")
-            #print ('Cation list is: ',Cations)
-        elif opt in ("-a","--Anions"):
+            Central = str(arg)
+        elif opt in ("-a","--Adjacent"):
             header = header + ' -a=' + arg
-            Anions= arg.split(",")
-            #print ('Anion list is: ',Anions)
+            Adjacent = str(arg)
         elif opt in ("-r","--rRings"):
             rings = int(arg)
             if rings == 0:
@@ -215,13 +135,11 @@ def main(argv):
         print ('the UMD files ',BondFile,' does not exist')            
         sys.exit()
 
-    for ii in range(len(Cations)):
-        ClusterAtoms.append(Cations[ii])
-    for ii in range(len(Anions)):
-        if Anions[ii] not in ClusterAtoms:
-            ClusterAtoms.append(Anions[ii])
-                  
-    (MyCrystal,OctIndexes,TimeStep)=prep_read(BondFile)    
+
+    CentMin,CentMax,OutMin,OutMax,MyCrystal,Bonds,BondsIndexes,TimeStep = umdpf.read_bonds(BondFile,Central,Adjacent)
+
+    ClusterAtoms=[Central,Adjacent]
+    print(ClusterAtoms)
 
     centralatoms = []
     outeratoms = []
@@ -231,11 +149,11 @@ def main(argv):
             if MyCrystal.elements[MyCrystal.typat[iatom]]==ClusterAtoms[jatom]:
                 ligands.append(iatom)              #contains the list with the index of the ligand atoms from the 0 ... natom
     for iatom in range(MyCrystal.natom):
-        for jatom in range(len(Cations)):
-            if MyCrystal.elements[MyCrystal.typat[iatom]]==Cations[jatom]:
+        for jatom in range(len(Central)):
+            if MyCrystal.elements[MyCrystal.typat[iatom]]==Central:
                 centralatoms.append(iatom)         #contains the list with the index of the central atoms from the 0 ... natom
-        for jatom in range(len(Anions)):
-            if MyCrystal.elements[MyCrystal.typat[iatom]]==Anions[jatom]:
+        for jatom in range(len(Adjacent)):
+            if MyCrystal.elements[MyCrystal.typat[iatom]]==Adjacent:
                 outeratoms.append(iatom)           #contains the list with the index of the coordinating atoms from the 0 ... natom
 
     print('All ligands are: ',ligands)
@@ -243,21 +161,14 @@ def main(argv):
     print('Coordinating atoms are :',outeratoms)
     
     if centralatoms == []:
-        print("ERROR : element ",Cations," not present in simulation")
+        print("ERROR : element ",Central," not present in simulation")
         sys.exit()
     if outeratoms == []:
-        print("ERROR : element ",Anions," not present in simulation")
+        print("ERROR : element ",Adjacent," not present in simulation")
         
-    readRed=partial(read_snapshotbonds_C,CentMin=centralatoms[0],CentMax=centralatoms[-1],OutMin=outeratoms[0],OutMax=outeratoms[-1],File=BondFile)
     
-
-    with concurrent.futures.ProcessPoolExecutor() as executor :
-        Data = list(executor.map(readRed,OctIndexes[:-1],OctIndexes[1:],[step for step in range(len(OctIndexes)-1)])) #Converting bond file into python list
-
-    Bonds = [Data[i][0] for i in range(len(Data))]
-    BondsIndexes = [Data[i][1] for i in range(len(Data))]#BondIndexes[x][i] indicates how many atoms, for the snapshot n° x, are listed in Bonds before the ones bound to the atom n° i. 
     
-    clusteringRed=partial(clustering, CentMin=centralatoms[0],CentMax=centralatoms[-1],OutMin=outeratoms[0],OutMax=outeratoms[-1],r=rings)
+    clusteringRed=partial(clustering, CentMin=CentMin,CentMax=CentMax,OutMin=OutMin,OutMax=OutMax,r=rings)
        
     with concurrent.futures.ProcessPoolExecutor() as executor :
         clusters=list(executor.map(clusteringRed,Bonds,BondsIndexes, [step for step in range(len(Bonds))])) #Computes the clusters of atoms for each snapshot separately
@@ -276,7 +187,7 @@ def main(argv):
     for pop in populations[1:]: #for each chunk
         for key in pop:
             if key in population.keys():#if the cluster has been previously seen
-                if population[key][-1][-1]==pop[key][0][0]-1:#if the cluster was alive at the end of the previous chung and at the beginning of this one
+                if population[key][-1][-1]==pop[key][0][0]-1:#if the cluster was alive at the end of the previous chunk and at the beginning of this one
                     population[key][-1]+=pop[key][0]
                     population[key]+=pop[key][1:]
                 else :
@@ -286,10 +197,10 @@ def main(argv):
 
                 
         #Creating the output files        
-    FileAll = BondFile[:-4] +'.r=' + str(rings) + '.popul.dat'
-    print ('Population will be written in ',FileAll,' file')
-    FileStat = BondFile[:-4] + '.r=' + str(rings) + '.stat.dat'
-    print ('Statistics will be written in ',FileStat,' file')
+    FileAll = BondFile[:-4] +'.r=' + str(rings) + '.populB.dat'
+#    print ('Population will be written in ',FileAll,' file')
+    FileStat = BondFile[:-4] + '.r=' + str(rings) + '.statB.dat'
+#    print ('Statistics will be written in ',FileStat,' file')
     header+="\n"            
                 
     print("Writing...")
