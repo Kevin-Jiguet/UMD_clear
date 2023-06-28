f139eee2b9f9dd4fb112cff11d79fdfca26b37af
Kevin-Jiguet <129768379+Kevin-Jiguet@users.noreply.github.com>
2023-06-28 10:50:45 +0200

    Update msd_umd_fast.py
    Adding the possibility for the user tho choose the directions over which to compute the msd.

--- new_UMD/msd_umd_fast.py
+++ new_UMD/msd_umd_fast.py
@@ -3,14 +3,14 @@
 
 
 import sys,getopt,os.path
-import crystallography as cr
-import umd_process as umdp
+import umd_processes_fast as umdpf
 import time
 import numpy as np
 import concurrent.futures
 from functools import partial
 import ctypes
 from os.path import join
+import math
 
 
 current_path=os.path.abspath(__file__)#For all this to work, the file c_msd.so must be in the same directory than this script
@@ -20,21 +20,47 @@ path_new=''
 for u in path_red:
     path_new+='/'+u
 msd_lib = ctypes.cdll.LoadLibrary(join(path_new, 'c_msd.so'))
-msd_lib.compute_msd.argtypes = [ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double), ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_int]
-msd_lib.compute_msd.restype = None
+msd_lib.compute_msd_tilted.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.POINTER(ctypes.c_double)]
+msd_lib.compute_msd_tilted.restype = ctypes.POINTER(ctypes.c_double)
+
+msd_lib.free_memory.argtypes = [ctypes.POINTER(ctypes.c_double)]
+msd_lib.free_memory.restype = None
+
+msd_lib.compute_msd.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.c_int,ctypes.c_int,ctypes.c_int,ctypes.c_int]
+msd_lib.compute_msd.restype = ctypes.POINTER(ctypes.c_double)
 
 
 
-def msdAtom_C(n,pos,hh,vv,ballistic):
-    PosAr=np.array(pos)
-    nitmax=(len(pos)//2-ballistic)
-    msd=np.array([0.0 for _ in range(nitmax//vv)])    
-    Posp = PosAr.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-    MSDp = msd.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-
-    msd_lib.compute_msd(Posp,MSDp,hh,vv,ballistic,nitmax)
+def msdAtom_C(n,PosAr,numsteps,hh,vv,ballistic):
+    t1=time.time()
+    nitmax=(numsteps//2-ballistic)
+    Posp = (PosAr.flatten()).ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    msd=[]
+    t2=time.time()
+    msdP=msd_lib.compute_msd(Posp,hh,vv,ballistic,nitmax)
+    for i in range(nitmax):
+        msd.append(msdP[i])
+    t3=time.time()
+    msd_lib.free_memory(msdP)
+    print("step "+str(n)+" : conversions "+str(t2-t1)+" s ; calculations "+str(t3-t2)+" s" )
     return msd
 
+def msdAtom_CAxes(n,PosAr,numsteps,hh,vv,ballistic,Axes):
+    t1=time.time()
+    nitmax=(numsteps//2-ballistic)
+    Axesp = (9 * ctypes.c_double)(*Axes)
+    Posp = (PosAr.flatten()).ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    msd=msd_lib.compute_msd_tilted(Posp,hh,vv,ballistic,nitmax,Axesp)
+    t2=time.time()    
+    msdA1,msdA2,msdA3 = [], [], []
+    for i in range(int(nitmax/vv)):
+        msdA1.append(msd[i])
+        msdA2.append(msd[i+int(nitmax/vv)])
+        msdA3.append(msd[i+2*int(nitmax/vv)])
+    msd_lib.free_memory(msd)
+    t3=time.time()
+    print("step "+str(n)+" : calc "+str(t2-t1)+" s ; copy "+str(t3-t2)+" s" )
+    return msdA1,msdA2,msdA3
 
 
 def main(argv):
@@ -42,25 +68,29 @@ def main(argv):
     vv = 1
     ballistic = 0
     TimeStep = 1
-    umdp.headerumd()
+    umdpf.headerumd()
     start=time.time()
     umdfile=''
+    Auto = True
+#    Axes=[1,0,0,0,1,0,0,0,1]
+    Axes = None
     try:
-        opts, arg = getopt.getopt(argv,"hf:z:v:b:x:",["fumdfile","zHorizontalJump","vVerticalJump","bBallistic","xAtoms"])
+        opts, arg = getopt.getopt(argv,"hf:z:v:b:m:a:",["fumdfile","zHorizontalJump","vVerticalJump","bBallistic","mMode","aAxes"])
     except getopt.GetoptError:
-        print ('msd_umd.py -f <XYZ_filename> -z <HorizontalJump> -v <VerticalJump> -b <Ballistic> -a <Atoms>')
+        print ('msd_umd.py -f <XYZ_filename> -z <HorizontalJump> -v <VerticalJump> -b <Ballistic> -m <Mode> -a <Axes>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
             print ('Program to compute the Mean Square Displacement. Usage: ')
-            print ('msd_umd.py -f <UMD_filename> -z <HorizontalJump> -v <VerticalJump> -b <Ballistic>')
+            print ('msd_umd_fast.py -f <UMD_filename> -z <HorizontalJump> -v <VerticalJump> -b <Ballistic> -x <Atoms>')
             print ('UMD_filename = input file with the trajectory, in UMD format.')
             print (' As the MSD is measured with a sliding window of various size up to half the trajectory\'s length, ')
             print (' we can accelerate the calculation using a selected reduced sampling ')
             print ('HorizontalJump = discretization for the start of the sampling window.')
             print ('VerticalJump = discretization for the length of the sampling window.')
             print ('Ballistic = estimation of the ballistic part of the trajectory. Default is 0. Typical values of 100 are sufficient.')
-            print ('Atoms = atoms or elements ; parameter to either print the msd of each individual atom (+ the mean msd of each element) or only the msd of the elements.')
+            print ('Mode = atoms or elements ; parameter to either print the msd of each individual atom (+ the mean msd of each element) or only the msd of the elements.')
+            print ('Axes = a list of float that describes the axes along which the msd should be calculated. If this argument is absent, the default will be the traditional cartesian coordinates, and the execution will be slightly faster. ')
             sys.exit()
         elif opt in ("-f", "--fumdfile"):
             umdfile = str(arg)
@@ -77,108 +107,138 @@ def main(argv):
                 sys.exit()
         elif opt in ("-b", "--bBallistic"):
             ballistic = int(arg)
-        elif opt in ("-x","--xAtoms"):
+        elif opt in ("-m","--mMode"):
             x=str(arg)
+        elif opt in ("-a","--aAxes"):
+            Axes = eval(arg)
+            
+            l1 = math.sqrt(Axes[0]**2+Axes[1]**2+Axes[2]**2)
+            l2 = math.sqrt(Axes[3]**2+Axes[4]**2+Axes[5]**2)
+            l3 = math.sqrt(Axes[6]**2+Axes[7]**2+Axes[8]**2)
+            #Normalization        
+            Axes[0]/=l1 
+            Axes[1]/=l1 
+            Axes[2]/=l1 
+            Axes[3]/=l2 
+            Axes[4]/=l2 
+            Axes[5]/=l2 
+            Axes[6]/=l3 
+            Axes[7]/=l3
+            Axes[8]/=l3 
+
             
     if (os.path.isfile(umdfile)):
         
-        MyCrystal=cr.Lattice()
-        ff=open(umdfile,'r')
-        while True :
-            line=ff.readline()
-            if not line: break
-            #print(line,len(line))
-            if len(line) > 1:
-                line=line.strip()
-                entry=line.split()
-                if entry[0] == 'natom':
-                    MyCrystal.natom = int(entry[1])
-                    MyCrystal.typat = [0 for _ in range(MyCrystal.natom)]
-                    dicoAtoms={at:[] for at in range(MyCrystal.natom)}
+        MyCrystal,SnapshotsValues,TimeStep,numsteps = umdpf.read_values(umdfile,"absxcart","chunks",1,"all")
+        print(len(SnapshotsValues))
 
-                if entry[0] == 'ntypat':
-                    MyCrystal.ntypat = int(entry[1])
-                    MyCrystal.types = [0 for _ in range(MyCrystal.ntypat)]
-                    MyCrystal.elements = ['X' for _ in range(MyCrystal.ntypat)]
-                    MyCrystal.masses = [1.0 for _ in range(MyCrystal.ntypat)]
-                    MyCrystal.zelec = [1.0 for _ in range(MyCrystal.ntypat)]
-                if entry[0] == 'types':
-                    for ii in range(MyCrystal.ntypat):
-                        MyCrystal.types[ii]=int(entry[ii+1])
-                if entry[0] == 'elements':
-                    for ii in range(MyCrystal.ntypat):
-                        MyCrystal.elements[ii]=entry[ii+1]
-                if entry[0] == 'masses':
-                    for ii in range(MyCrystal.ntypat):
-                        MyCrystal.masses[ii]=float(entry[ii+1])
-                if entry[0] == 'Zelectrons':
-                    for ii in range(MyCrystal.ntypat):
-                        MyCrystal.zelec[ii]=float(entry[ii+1])
-                if entry[0] == 'typat':
-                    for ii in range(MyCrystal.natom):
-                        MyCrystal.typat[ii] = int(entry[ii+1])
-                if entry[0] == 'timestep':
-                    TimeStep = float(entry[1])
-                if entry[0] == 'atoms:':
-                    for iatom in range(MyCrystal.natom):
-                        line=ff.readline()
-                        line=line.strip()
-                        entry=line.split()
-                        dicoAtoms[iatom].append([float(entry[6]),float(entry[7]),float(entry[8])])
-                        
-        print ('Number of atoms of each type is ',MyCrystal.types)
+        if Auto :
+            Axes = MyCrystal.rprim[0] + MyCrystal.rprim[1] + MyCrystal.rprim[2]    
+
         
+        ArrayAbsXcart = np.array(SnapshotsValues).flatten()
+
+        ListAbsXcart = [ArrayAbsXcart[at::MyCrystal.natom] for at in range(MyCrystal.natom)]
+
+        
+
+        print ('Number of atoms of each type is ',MyCrystal.types)
         with concurrent.futures.ProcessPoolExecutor() as executor:
-            msdAtomRed=partial(msdAtom_C,hh=hh,vv=vv,ballistic=ballistic)
-            msdfile = umdfile[:-8] + '.msd_par'
-                
-               
-            msdArray=list(executor.map(msdAtomRed,[iatom for iatom in range(MyCrystal.natom)],[dicoAtoms[iatom] for iatom in range(MyCrystal.natom)]))
-                
+            if Axes !=None :
+                msdAtomRed=partial(msdAtom_CAxes,numsteps=numsteps,hh=hh,vv=vv,ballistic=ballistic,Axes=Axes)
+            else :
+                msdAtomRed=partial(msdAtom_C,numsteps=numsteps,hh=hh,vv=vv,ballistic=ballistic)
+
+            msdfile = umdfile[:-8] + '.msd_fasterA'
+                              
+            msdArray=list(executor.map(msdAtomRed,[iatom for iatom in range(MyCrystal.natom)],[ListAbsXcart[iatom] for iatom in range(MyCrystal.natom)]))
+            headerstring='MSD : -f '+umdfile+' -m '+ x +'-a '+str(Axes)+'\n'
             if(x=="elements"):
                 
-                MSD=np.array([[0.0 for _ in range(len(msdArray[0]))] for _ in range(MyCrystal.ntypat)])
-                for iatom in range(len(msdArray)) :
-                    MSD[MyCrystal.typat[iatom]]+=msdArray[iatom]
+                if Axes != None :
+                    msdfile+='.axes'
+                    MSD=np.array([[np.zeros(len(msdArray[0][0])),np.zeros(len(msdArray[0][0])),np.zeros(len(msdArray[0][0])),np.zeros(len(msdArray[0][0]))] for _ in range(MyCrystal.ntypat)])
+                    for iatom in range(len(msdArray)) :
+                        for kk in range(3):
+                            MSD[MyCrystal.typat[iatom]][1+kk]+=msdArray[iatom][kk]
+                            MSD[MyCrystal.typat[iatom]][0]+=msdArray[iatom][kk]
+                else :
+                    MSD=np.array([[np.zeros(len(msdArray[0]))] for _ in range(MyCrystal.ntypat)])
+                    for iatom in range(len(msdArray)) :
+                        MSD[MyCrystal.typat[iatom]][0]+=msdArray[iatom]
+                        
                 msdfile+='.elements.dat'
                 f = open(msdfile,'w')
+                f.write(headerstring)
                 string='time_(fs)\t'
                 for itypat in range(MyCrystal.ntypat):
-                    string=string + MyCrystal.elements[itypat] + '\t'
+                    string +=  MyCrystal.elements[itypat] + '\t' + MyCrystal.elements[itypat] + ' (1)\t' + MyCrystal.elements[itypat] + '(2)\t'+ MyCrystal.elements[itypat] + '(3)\t'
                 string = string + '\n'
                 f.write(string)
-                niter=len(dicoAtoms[0])
  #                   print(niter, hh, ballistic)
-                weight=int((int(niter/2)-ballistic)/hh)-1
+                weight=int((int(numsteps/2)-ballistic)/hh)-1
  #                   print("weight=",weight*MyCrystal.natom)                    
-                for ii in range(len(MSD[0])):     
+                for ii in range(len(MSD[0][0])):     
                     instant = (float(ii)*TimeStep*vv)+ballistic
                     string = str(instant)
                     for jj in range(MyCrystal.ntypat):
-                        string = string + '\t' + str(MSD[jj][ii]/(float(MyCrystal.types[jj])*float(weight)))
+                        string+='\t' + str(MSD[jj][0][ii]/(float(MyCrystal.types[jj])*float(weight)))
+                        if Axes != None :
+                            for kk in range(1,4):
+                                string += '\t' + str(MSD[jj][kk][ii]/(float(MyCrystal.types[jj])*float(weight)))
                     string = string + '\n'
                     f.write(string)
                 print ('MSDs printed in file ',msdfile)
-   
+                
             elif(x=="atoms"):
                 
+                if Axes != None :
+                    msdfile+=".axes"
+                    MSD=np.array([[np.zeros(len(msdArray[0][0])),np.zeros(len(msdArray[0][0])),np.zeros(len(msdArray[0][0])),np.zeros(len(msdArray[0][0]))] for _ in range(MyCrystal.natom)])
+                    for iatom in range(len(msdArray)) :
+                        for kk in range(3):
+                            MSD[iatom][1+kk]=msdArray[iatom][kk]
+                            MSD[iatom][0]+=msdArray[iatom][kk]
+                else :
+                    MSD=np.array([[np.zeros(len(msdArray[0]))] for _ in range(MyCrystal.natom)])
+                    for iatom in range(len(msdArray)) :
+                        MSD[iatom][0] = msdArray[iatom]
                 msdfile+='.atoms.dat'
                 f = open(msdfile,'w')
+                f.write(headerstring)
                 string='time_(fs)\t'
-                niter=len(dicoAtoms[0])
-                weight=int((int(niter/2)-ballistic)/hh)-1
-                string='time (fs)'
+                lastEl=''
                 for iatom in range(MyCrystal.natom):
-                    string+='\t'+MyCrystal.elements[MyCrystal.typat[iatom]]+str(iatom)
-                f.write(string+'\n')
-                for ii in range(len(msdArray[0])):     
+                    if lastEl!=MyCrystal.elements[MyCrystal.typat[iatom]]:
+                        indexat = 0
+                        lastEl=MyCrystal.elements[MyCrystal.typat[iatom]]
+                    label = lastEl+str(indexat)
+                    if Axes != None :
+                        string +=  label + '\t' + label + '(1)\t' + label + '(2)\t'+ label + '(3)\t'
+                    else :
+                        string += label +'\t'
+                    
+                    indexat+=1
+                    
+                string = string + '\n'
+                f.write(string)
+
+                weight=int((int(numsteps/2)-ballistic)/hh)-1
+
+                for ii in range(len(MSD[0][0])):     
                     instant = (float(ii)*TimeStep*vv)+ballistic
                     string = str(instant)
-                    for iatom in range(MyCrystal.natom):
-                        string = string + '\t' + str(msdArray[iatom][ii]/(float(weight)))
-                    string = string + '\n'
+                    for jj in range(MyCrystal.natom):
+                        string+='\t' + str(MSD[jj][0][ii]/(float(weight)))
+                        if Axes != None :
+                            for kk in range(1,4):
+                                string += '\t' + str(MSD[jj][kk][ii]/(float(weight)))
+                    string +='\n'
                     f.write(string)
+
+
                 print ('MSDs printed in file ',msdfile)
+   
          
     else:
         print ('umd file ',umdfile,'does not exist')
