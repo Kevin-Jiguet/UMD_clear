980ae35b48affb84585afe9d98a5a1ad293a7dd6
Kevin-Jiguet <129768379+Kevin-Jiguet@users.noreply.github.com>
2023-07-03 11:34:47 +0200

    Update c_clusters_B.c

--- new_UMD/c_clusters_B.c
+++ new_UMD/c_clusters_B.c
@@ -9,6 +9,10 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+void free_memory(int *tab){
+	free(tab);
+}
+
 int min(int a,int b){
 
 	if(a>b){return b;}
@@ -30,26 +34,30 @@ double min3(double a, double b, double c){
 
 double* read_coord(char *MySnapshot, int nAtoms, int len){//reads the xcart coordinates of a snapshot and returns it in a tab
 
-	int depth = 0;
+	int depth = 1;
 	int atom = 0;
 	int flagxcart = 0;
 	double number = 0;
 	double* Coordinates;
-	int lineIndex=0;//index on the line of the number we are looking at
+	int lineIndex=0;
+
 	Coordinates = calloc(3*nAtoms,sizeof(double));
+	
 	for(int i=0 ; i<len ; i++){
 
-		if(MySnapshot[i]==' '){	
+		if(MySnapshot[i]=='\t'){
+		
+
 			if(flagxcart){
-				Coordinates[3*atom+lineIndex-3]=number;
+				Coordinates[3*atom+lineIndex-3]=42+0*number;
 				flagxcart=0;
 			}
 			number=0;
 			lineIndex++;
-			depth=0;//The next digits will be in the integer part
+			depth=1;
 		}
 
-		else if(lineIndex==3||lineIndex==4||lineIndex==5){//If we are on the xcart coordinates
+		else if(lineIndex==3||lineIndex==4||lineIndex==5){
 			flagxcart = 1;
 			if(isdigit(MySnapshot[i])){
 				if(depth == 0){
@@ -58,111 +66,89 @@ double* read_coord(char *MySnapshot, int nAtoms, int len){//reads the xcart coor
 				}
 			
 				else{
-					number=number+(double)(MySnapshot[i]-'0')/depth;
-					depth=depth*10;
-
+					depth++;
+					number=number+(MySnapshot[i]-'0')/depth;
 				}
-			}
-			else if(MySnapshot[i]=='.'){
-				depth=10;//We're in the decimal part now
 			}
 			
 
 		}
 		else if(MySnapshot[i]=='\n'){
-			atom++;//Next line, next atom ; we reinitialize the variables
+			atom++;
 			lineIndex=0;
-			depth=0;
+			depth=1;
 			number=0;
 		}
 	}
 return Coordinates;
 }
 
-//Calculates the angles within the polyhedras if it contains at least 2 coordinated atoms
-double* angles(const int *polyhedras, int nPol, int M, const double *MySnapshot, int CentMin, int CentMax, int OutMin, int OutMax, double acell0, double acell1, double acell2){
-	
+double* angles(int *polyhedras, int nPol, int M, int *MySnapshot, int CentMin, int CentMax, int OutMin, int OutMax, double acell0, double acell1, double acell2){
+
 	double* AnglesList;
 	int* OutList;
-	double* CoordList;
+	int* CoordList;
 	int at,ne1,ne2;	
-	double xCent,yCent,zCent;
-	double Deltx1,Delty1,Deltz1,Deltx2,Delty2,Deltz2,valx1,valy1,valz1,valx2,valy2,valz2,deltzz,deltxx,deltyy,valxx,valyy,valzz,dist1,dist2,distij,angle;
-	double x1,y1,z1,x2,y2,z2;
+	int xCent,yCent,zCent,Deltx1,Delty1,Deltz1,Deltx2,Delty2,Deltz2,valx1,valy1,valz1,valx2,valy2,valz2,deltzz,deltxx,deltyy,valxx,valyy,valzz,dist1,dist2,distij,angle;
 	int centat=1;
 	int nbounds=0 ;
 	int ncent=0;
-	int index=0;
 	int flagcoord=0;
 
-	CoordList = calloc(3*M,sizeof(double));
-	OutList = calloc(M,sizeof(int));
+
+	CoordList = calloc(M,sizeof(int));
+	OutList = calloc(3*M,sizeof(int));
 	AnglesList = calloc((OutMax-OutMin+1)*(OutMax-OutMin)/2+1+CentMax-CentMin+1,sizeof(double));
 	AnglesList[0]=1;
 
 
 
 	while(ncent<nPol){
-		
-		index++;
-		at = polyhedras[index];
-		
-		
-		if(at==-1){
-			centat = 1;//means that the next atom on the polyhedras list will be a central atom
-			nbounds = 0;
-			ncent++;
-			AnglesList[(int)AnglesList[0]]=-1;//separation between clusters
-			AnglesList[0]++;
 
+		at = polyhedras[ncent];
+		ncent++;
+		if(at==-1){
+			AnglesList[(int)AnglesList[0]]=-1;
+			AnglesList[0]++;
+			centat=1;
+			nbounds = 0;
 		}		
 		
-		else if(centat==1){//we define the coordinates of the central atom
-			centat = 0;//The next atom on the polyhedras list will be an adjacent atom
+		else if(centat==1){
+			centat=0;
 			xCent = MySnapshot[3*at];
 			yCent = MySnapshot[3*at+1];
 			zCent = MySnapshot[3*at+2];
 		}
-
-		else {
+		else{
 			flagcoord=1;
-			while(flagcoord){//while we are looking at the same cluster
-				at = polyhedras[index];
-				if(at!=-1){
-					CoordList[3*nbounds]=MySnapshot[3*at];//We fill the CoordList with the coordinates of outer atoms
-					CoordList[3*nbounds+1]=MySnapshot[3*at+1];
-					CoordList[3*nbounds+2]=MySnapshot[3*at+2];
-					nbounds++;
-					index++;
-				}
-				else{
-					flagcoord=0;//Means that we're out of the cluster now
-					index--;
+			while(flagcoord){
+				OutList[nbounds]=polyhedras[ncent];
+				CoordList[3*nbounds]=MySnapshot[3*OutList[nbounds]];
+				CoordList[3*nbounds+1]=MySnapshot[3*OutList[nbounds]+1];
+				CoordList[3*nbounds+2]=MySnapshot[3*OutList[nbounds]+2];
+				nbounds++;
+				ncent++;				
+				if(polyhedras[ncent]==-1){
+					flagcoord=0;
 				}
 			}
-			for(int i=0 ; i<nbounds ; i++){//We browse the CoordList to compute the angle for each pair of outer (adjacent) atoms
-
-				x1 = CoordList[3*i];
-				y1 = CoordList[3*i+1];
-				z1 = CoordList[3*i+2];
+			
+			for(int i=0 ; i<nbounds ; i++){
 								
+
 				for(int j=i+1 ; j<nbounds ; j++){
+					Deltx1 = CoordList[3*i]-xCent;
+					Delty1 = CoordList[3*i+1]-yCent;
+					Deltz1 = CoordList[3*i+2]-zCent;
 
-					x2 = CoordList[3*j];
-					y2 = CoordList[3*j+1];
-					z2 = CoordList[3*j+2];
+					Deltx2 = CoordList[3*j]-xCent;
+					Delty2 = CoordList[3*j+1]-yCent;
+					Deltz2 = CoordList[3*j+2]-zCent;
 
-					Deltx1 = x1-xCent;
-					Delty1 = y1-yCent;
-					Deltz1 = z1-zCent;
-
-					Deltx2 = x2-xCent;
-					Delty2 = y2-yCent;
-					Deltz2 = z2-zCent;
-
-					deltxx = x1-x2; 					
-					deltyy = y1-y2; 					
-					deltzz = z1-z2;					
+					deltxx = CoordList[3*i]-CoordList[3*j]; 					
+					deltyy = CoordList[3*i+1]-CoordList[3*j+1]; 					
+					deltzz = CoordList[3*i+2]-CoordList[3*j+2];					
 					
 					valx1=min3(pow(Deltx1,2),pow(Deltx1-acell0,2),pow(Deltx1+acell0,2));													
 					valy1=min3(pow(Delty1,2),pow(Delty1-acell1,2),pow(Delty1+acell1,2));
@@ -181,21 +167,18 @@ double* angles(const int *polyhedras, int nPol, int M, const double *MySnapshot,
 					valzz = min3(pow(deltzz,2),pow(deltzz-acell2,2),pow(deltzz+acell2,2));
 
 					distij = valxx+valyy+valzz;
-
 					
-					if(dist1==0||dist2==0){
-					AnglesList[(int)AnglesList[0]]=0.0;
-					}
-					else{
-					AnglesList[(int)AnglesList[0]] = acos((dist1+dist2-distij)/(2*sqrt(dist1*dist2)))*180/3.141592653589793;
-					}
+					AnglesList[(int)AnglesList[0]] = acos((dist1+dist2-distij)/2);
 					AnglesList[0]++;
 				}
 			}
 		}
 	}
+
 	return AnglesList;
-} int atom_to_indexB(int atom,int CentMin,int CentMax,int OutMin,int OutMax){//Gives, in the BondIndexes tab, the index of the data pertaining to the atom.
+} 
+
+int atom_to_indexB(int atom,int CentMin,int CentMax,int OutMin,int OutMax){
 
 	int place;
 	if(CentMax<=OutMax){
@@ -216,7 +199,7 @@ double* angles(const int *polyhedras, int nPol, int M, const double *MySnapshot,
 	}
 }
 
-int atom_to_indexAt(int atom,int CentMin,int CentMax,int OutMin,int OutMax){//Gives, in the AllAtoms tab, the index of the atom.
+int atom_to_indexAt(int atom,int CentMin,int CentMax,int OutMin,int OutMax){
 
 	int indice;
 	if(atom<=CentMax && atom>=CentMin){	
@@ -229,7 +212,7 @@ int atom_to_indexAt(int atom,int CentMin,int CentMax,int OutMin,int OutMax){//Gi
 	return indice;
 }
 
-void reinit(int *AllAts,int CentMin, int CentMax,int OutMin,int OutMax){//Reinitializes (refills) the AllAts list with all the atoms.
+void reinit(int *AllAts,int CentMin, int CentMax,int OutMin,int OutMax){
 
 	if(CentMax!=OutMax){
 		for(int i=0 ; i<(CentMax-CentMin+1) ; i++){
@@ -247,7 +230,7 @@ void reinit(int *AllAts,int CentMin, int CentMax,int OutMin,int OutMax){//Reinit
 }
 
 
-void clusteringall(int *neighbors,const int *SnapshotBonds, const int *indBonds,int *AllAtoms, const int CentMin, const int CentMax, const int OutMin, const int OutMax, const int atom){//fills the neighbors tab with the atoms of each cluster. Polymerization.
+void clusteringall(int *neighbors,const int *SnapshotBonds, const int *indBonds,int *AllAtoms, const int CentMin, const int CentMax, const int OutMin, const int OutMax, const int atom){
 
 	int indiceAt, at, indiceCoord;
 	indiceAt = atom_to_indexB(atom,CentMin,CentMax,OutMin,OutMax);
@@ -265,7 +248,7 @@ void clusteringall(int *neighbors,const int *SnapshotBonds, const int *indBonds,
 	}
 }
 
-void clusteringrec(int *neighbors,const int *SnapshotBonds, const int *indBonds,int *AllAtoms, const int CentMin, const int CentMax, const int OutMin, const int OutMax, const int atom, const int r){//fills the neighbors tab with the atoms of each cluster. Coordination.
+void clusteringrec(int *neighbors,const int *SnapshotBonds, const int *indBonds,int *AllAtoms, const int CentMin, const int CentMax, const int OutMin, const int OutMax, const int atom, const int r){
 
 	if(r>0){
 
@@ -287,7 +270,7 @@ void clusteringrec(int *neighbors,const int *SnapshotBonds, const int *indBonds,
 	}
 }
 
-int* fullclustering(const int *SnapshotBonds, const int *indBonds, const int CentMin, const int CentMax, const int OutMin, const int OutMax, const int M, const int r){//returns the tab Neighbors, containing all the clusters, each separated by the value "-1".
+int* fullclustering(const int *SnapshotBonds, const int *indBonds, const int CentMin, const int CentMax, const int OutMin, const int OutMax, const int M, const int r){
 	
 	int *AllAts;
 	int *neighbors;
@@ -309,8 +292,6 @@ int* fullclustering(const int *SnapshotBonds, const int *indBonds, const int Cen
 		len = 2*nAts+1;					
 	}
 
-//Memory allocation
-	
 	neighbors = calloc(len,sizeof(int));
 	AllAts = calloc(nAts,sizeof(int));		
 
@@ -326,7 +307,7 @@ int* fullclustering(const int *SnapshotBonds, const int *indBonds, const int Cen
 //Neighbors computation
 	neighbors[0]=1;
 
-	if(r>0){//Coordination
+	if(r>0){
 		while(index<CentMax-CentMin+1){
 			if(AllAts[index] != -1){
 				atom=AllAts[index];
@@ -337,10 +318,10 @@ int* fullclustering(const int *SnapshotBonds, const int *indBonds, const int Cen
 			}		
 			neighbors[0]++;
 			index++;
-			reinit(AllAts,CentMin,CentMax,OutMin,OutMax);//Reinitialization because the same coordinating atom can be in different clusters
+			reinit(AllAts,CentMin,CentMax,OutMin,OutMax);	
 		}
 	}
-	else{//Polymerization
+	else{
 		while(index<nAts){
 			if(AllAts[index] != -1){
 				atom=AllAts[index];
@@ -351,7 +332,7 @@ int* fullclustering(const int *SnapshotBonds, const int *indBonds, const int Cen
 				neighbors[0]++;
 			}					
 			index++;		
-		}//No reinitialization because the same atom cannot be in two different polymers
+		}
 		while(neighbors[neighbors[0]]==-1){
 			neighbors[0]--;
 		}
@@ -362,11 +343,11 @@ int* fullclustering(const int *SnapshotBonds, const int *indBonds, const int Cen
 		return neighbors;
 	}
 	
-	Neighbors = calloc(neighbors[0]+1,sizeof(int));//More condensed data
+	Neighbors = calloc(neighbors[0]+1,sizeof(int));
 		
 	int ind=1;
 	len=0;
-//Putting only the relevant data in the Neighbors tab
+
 	for(int i=1 ; i<(neighbors[0]+1) ; i++){
 		if(neighbors[i]==-1){
 			if(len<=1){
@@ -398,5 +379,6 @@ int* fullclustering(const int *SnapshotBonds, const int *indBonds, const int Cen
 	free(AllAts);
 
 	return Neighbors;
+
 }
 	
